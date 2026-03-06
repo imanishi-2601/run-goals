9ab0d602cba3006cbb791d5ede7ace1039f99911
Ubuntu <ubuntu@ip-172-31-20-231.ap-northeast-1.compute.internal>
2026-03-06 13:27:10 +0000

    [WIP]top,about,communities作成

---
+++ app/controllers/communities_controller.rb
@@ -0,0 +1,9 @@
+class CommunitiesController < ApplicationController
+  #before_action :set_book, only: [:show, :edit, :update, :destroy]
+  #before_action :require_owner!, only: [:edit, :update, :destroy]
+
+  def index
+    #@user     =Current.session.user
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  root "homes#top"
+  get "homes/top"
+  get "about" => "homes#about"
+
+  # =========================================================
+  # ユーザー
+  # =========================================================
+  devise_for :users
+  resources :communities
+
+    # customers（マイページ・編集・退会）
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
