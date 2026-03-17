1daa3f7331529adc674ef2cac783c59f60066ee4
Ubuntu <ubuntu@ip-172-31-34-237.ap-northeast-1.compute.internal>
2026-03-17 12:08:21 +0000

    ログイン後遷移先修正、誤記訂正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_in_path_for(resource)
-    communities_path
+    user_path(resource)
   end
 
   def after_sign_up_path_for(resource)
-    communities_path
+    user_path(resource)
   end
 
 end
