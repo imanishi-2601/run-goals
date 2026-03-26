cb6c510ce8d5069d0545b21ecc4fdd784bb3b702
Ubuntu <ubuntu@ip-172-31-34-237.ap-northeast-1.compute.internal>
2026-03-26 03:06:57 +0000

    管理者ログイン機能・ログアウト遷移の修正

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -1,13 +1,20 @@
-class Admins::SessionsController < ApplicationController
-  def new
-    super
-  end
+class Admins::SessionsController < Admins::BaseController
+  skip_before_action :authenticate_admin!, only: [:new, :create, :destroy]
 
   def create
-    super
+    user = User.find_by(email: params[:email])
+
+    if user&.valid_password?(params[:password]) && user.admin?
+      sign_in(user)
+      redirect_to admins_root_path   # ← noticeを書かない
+    else
+      flash.now[:alert] = "メールアドレスまたはパスワードが正しくありません。"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
-    super
+    sign_out(:user)
+    redirect_to admins_sign_in_path, notice: "ログアウトしました。"
   end
 end
